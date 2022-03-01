3b7b6c1288a17adb5b704fbeb55f3804a8118a37
erik savage <erik.k.savage@gmail.com>
2022-03-01 15:08:38 -0800

    fixes dispatch, now sends whole category object

--- src/components/storefront/Categories.jsx
+++ src/components/storefront/Categories.jsx
@@ -1,26 +1,16 @@
 import React from "react";
 import {connect} from 'react-redux';
 import { nanoid } from "nanoid";
-import { useDispatch } from "react-redux";
 import {addCategory} from '../../store/categories.js'
 import {selectCategory} from '../../store/categories.js'
 
 function Categories(props){
-  console.log(props.categories);
-  const dispatch = useDispatch();
-  console.log('DISPATCH', dispatch);
-  const handleClick = (event) =>{
-    console.log(event.target.textContent);
-    console.log(props);
-    dispatch(selectCategory(event.target.textContent));
-    // props.store.dispatch(selectCategory(event.target.textContent));
-  }
-  
+
   return(
    <div>
       <h2>Browse These Categories...</h2>
       {props.categories.map(category => 
-      <span style={{margin: 15}} key={nanoid()} onClick={handleClick}>{category.displayName}</span>
+      <span style={{margin: 15}} key={nanoid()} onClick={() => props.selectCategory(category)}>{category.displayName}</span>
       )}
    </div>
 
--- src/store/categories.js
+++ src/store/categories.js
@@ -5,19 +5,18 @@ const intialState = {
     { normalizedName: 'electronics', displayName: 'ELECTRONICS', description: 'We Buy And Sell Goods' },
     { normalizedName: 'food', displayName: 'FOOD', description: 'No Food For You!' },
   ],
-  selected: null,
+  selectedCategory: null,
 }
 
 const categoryReducer = (state = intialState, action) => {
   let { type, payload } = action;
-  console.log("IN CATEGORY REDUCER");
   switch (type) {
     case 'ADD_CATEGORY':
       return { ...state, categories: [...state.categories, payload] }
     case 'SELECT_CATEGORY':
       console.log('clicked');
       console.log('PAYLOAD', payload);
-      return { ...state, selected: payload }
+      return { ...state, selectedCategory: payload.normalizedName }
     default:
       return state
   }
@@ -31,7 +30,7 @@ export const addCategory = (category) => {
 }
 
 export const selectCategory = (category) => {
-  console.log('testing');
+  console.log('testing payload', category);
   return {
     type: 'SELECT_CATEGORY',
     payload: category
