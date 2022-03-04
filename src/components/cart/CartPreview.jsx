14f9e32fc3fa93b03237eebd4aef96b9b3919222
erik savage <erik.k.savage@gmail.com>
2022-03-04 13:53:16 -0800

    deletes unused connect import

--- src/components/cart/CartPreview.jsx
+++ src/components/cart/CartPreview.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { connect } from "react-redux";
 import { useSelector } from "react-redux";
 import { nanoid } from "nanoid";
 import CartPreviewItem from "./CartPreviewItem";
